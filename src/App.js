2071336ebb955ac2e553d391ad37ef73b67cd564
ferrari219@nate.com <ferrari219@nate.com>
2021-07-13 00:01:37 +0900

    Done

--- src/App.js
+++ src/App.js
@@ -1,7 +1,38 @@
-import React from 'react';
+import React, { useReducer } from 'react';
 
+const initialState = {
+	counter: 0,
+};
+
+const INCREMENT = 'INCREMENT';
+const DECREMENT = 'DECREMENT';
+const reducer = (state, action) => {
+	switch (action.type) {
+		case INCREMENT:
+			return { counter: state.counter + 1 };
+		case DECREMENT:
+			return { counter: state.counter - 1 };
+		default:
+			return;
+	}
+};
 const App = () => {
-	return <div>App</div>;
+	const [state, dispatch] = useReducer(reducer, initialState);
+	const handleIncrement = (e) => {
+		e.preventDefault();
+		dispatch({ type: INCREMENT });
+	};
+	const handleDecrement = (e) => {
+		e.preventDefault();
+		dispatch({ type: DECREMENT });
+	};
+	return (
+		<div>
+			<h1>{state.counter}</h1>
+			<button onClick={handleIncrement}>+</button>
+			<button onClick={handleDecrement}>-</button>
+		</div>
+	);
 };
 
 export default App;
